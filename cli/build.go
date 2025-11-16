2220a9751399838110a62118b995420a7935c61c
Alexis Tan <alexis@sen-dev.com>
2025-11-16 15:41:20 +0100

    Fix build command setting up file descriptor context correctly

--- cli/build.go
+++ cli/build.go
@@ -20,7 +20,11 @@ var buildCommand = &cobra.Command{
 	Long:  "Compiles the specified Zen source file and outputs the bytecode to a .zenb file.",
 	Args:  cobra.ExactArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
-		inputFile := args[0]
+		inputFile, err := filepath.Abs(args[0])
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
 
 		content, err := os.ReadFile(inputFile)
 		if err != nil {
